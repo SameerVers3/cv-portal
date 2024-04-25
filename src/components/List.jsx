e7066454dfbc135e1623568d5764a19093685d5c
Muhammad Owais Muhammad Rafiq <137029948+owaisrafiq05@users.noreply.github.com>
2024-04-25 07:58:47 +0000

    form update 2

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -11,34 +11,101 @@ const Company = [
         id: "2",
         name: "Facebook",
         location: "USA",
-    }
-
+    },
+    {
+        id: "2",
+        name: "Facebook",
+        location: "USA",
+    },
+    {
+        id: "2",
+        name: "Facebook",
+        location: "USA",
+    },
+    {
+        id: "2",
+        name: "Facebook",
+        location: "USA",
+    },
+    {
+        id: "2",
+        name: "Facebook",
+        location: "USA",
+    },
+    {
+        id: "2",
+        name: "Facebook",
+        location: "USA",
+    },
+    {
+        id: "2",
+        name: "Facebook",
+        location: "USA",
+    },
+    {
+        id: "2",
+        name: "Facebook",
+        location: "USA",
+    },
+    {
+        id: "2",
+        name: "Facebook",
+        location: "USA",
+    },
+    {
+        id: "2",
+        name: "Facebook",
+        location: "USA",
+    },
 
 ]
 
 const List = () => {
-    return (
-        <div>
-            <div className="flex justify-center items-center flex-col w-screen h-screen gap-y-8 bg-gradient-to-b from-[#FFFFFF] to-[#9BD7E6]">
-                <h1 className="font-bold text-7xl text-[#23B6DF] text-center">Developer's Day 2024</h1>
-            {/* add somethign here */}
-            {Company.map((company) => {
-                return (
+    // return (
+    //     <div className="bg-gradient-to-b from-[#FFFFFF] to-[#9BD7E6]  my-8">
+    //     <div className="w-full h-full border">
+    //         <div className="flex justify-center items-center flex-col w-screen h-screen gap-y-8 ">
+    //             <h1 className="font-bold text-7xl text-[#23B6DF] text-center">Developer's Day 2024</h1>
+    //         {/* add somethign here */}
+    //         {Company.map((company) => {
+    //             return (
                     
-                    <Link to={`/job/${company.id}`} key={company.id} className="border">
+    //                 <Link to={`/job/${company.id}`} key={company.id} className="">
                         
-                        <div className="w-[40vw] justify-center items-center">
-                            <button type="submit"  className="w-full py-2 bg-white text-[#23B6DF]  text-3xl font-bold rounded-full hover:bg-gray-100">
-                            {company.name}
-                        {/* <h2>{company.location}</h2> */}
-                            </button>
-                        </div>
-                    </Link>
-                )
-            })}
+    //                     <div className="w-[40vw] justify-center items-center">
+    //                         <button type="submit"  className="w-full py-2 bg-white text-[#23B6DF]  text-3xl font-bold rounded-full hover:bg-gray-100">
+    //                         {company.name}
+    //                     {/* <h2>{company.location}</h2> */}
+    //                         </button>
+    //                     </div>
+    //                 </Link>
+    //             )
+    //         })}
+    //         </div>
+    //     </div>
+    //     </div>
+    // )
+    return (
+        <div className="bg-gradient-to-b from-[#FFFFFF] to-[#9BD7E6] h-screen overflow-hidden border-0">
+            <div className="w-full h-full border py-12">
+                <div className="flex items-center flex-col w-screen h-full gap-y-8 overflow-y-auto">
+                    <h1 className="font-bold text-7xl text-[#23B6DF] text-center">Developer's Day 2024</h1>
+                    {/* add something here */}
+                    {Company.map((company) => {
+                        return (
+                            <Link to={`/job/${company.id}`} key={company.id} className="w-[40vw]">
+                                <button type="submit" className="w-full py-2 bg-white text-[#23B6DF] text-3xl font-bold rounded-full hover:bg-gray-100">
+                                    {company.name}
+                                    {/* <h2>{company.location}</h2> */}
+                                </button>
+                            </Link>
+                        )
+                    })}
+                </div>
             </div>
         </div>
-    )
+    );
+    
 }
 
 export default List
